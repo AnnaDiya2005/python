5642da84090ec5395a69ec19f29d59f58dbf23f2
AnnaDiya2005 <annadiyasaji@gmail.com>
2025-05-13 23:07:45 +0530

    Add files via upload

---
+++ addition calculator.py
@@ -0,0 +1,5 @@
+print("additional calculator")
+num1=int(input("enter the number"))
+num2=int(input("enter the number"))
+sum=num1+num2
+print(f"sum is {num1}+{num2}={sum}")
---
+++ multiplication table.py
@@ -0,0 +1,5 @@
+print("multiplication table")
+num=int(input("enter the number"))
+for i in range(1,11):
+  print(f" {i} x {num}= {i*num}")
+
