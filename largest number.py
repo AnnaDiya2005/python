6eee8538e7483f4bb382bd6f49c15a7f4370a168
AnnaDiya2005 <annadiyasaji@gmail.com>
2025-05-15 09:56:29 +0530

    Update largest number.py

--- largest number.py
+++ largest number.py
@@ -5,7 +5,7 @@ if num1<num2:
     print(f"{num2} is greater")
 elif num2<num3:
     print(f"{num3} is greater")
-else:
-    print(f"{num1} is greater")
+elif num1<num3:
+    print(f"{num3} is greater")
 
     
